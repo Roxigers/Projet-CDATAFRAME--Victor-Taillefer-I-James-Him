d6164348ce8359e42c0bb36e4bcb8c44e42f5b5f
Roxigers <james.him@efrei.net>
2024-03-28 15:44:31 +0100

    test

--- functions.c
+++ functions.c
@@ -1,5 +1,9 @@
 //
 // Created by HIM on 28/03/2024.
 //
-
 #include "functions.h"
+
+COLUMN *create_column(char* title)
+{
+    char* title = calloc(n,sizeof(COLUMN));
+}
--- functions.h
+++ functions.h
@@ -4,5 +4,12 @@
 
 #ifndef PROJET_CDATAFRAME__VICTOR_TAILLEFER_I_JAMES_HIM_FUNCTIONS_H
 #define PROJET_CDATAFRAME__VICTOR_TAILLEFER_I_JAMES_HIM_FUNCTIONS_H
+/**
+ * Create a column
+ * @param1 : Column title
+ * @return : Pointer to created column
+ */
+
+COLUMN *create_column(char* title);
 
 #endif //PROJET_CDATAFRAME__VICTOR_TAILLEFER_I_JAMES_HIM_FUNCTIONS_H
