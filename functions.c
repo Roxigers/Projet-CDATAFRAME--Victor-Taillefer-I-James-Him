d8b6ae1de03ef6447c845a9e3d8be589828ec62d
Roxigers <james.him@efrei.net>
2024-03-30 18:07:35 +0100

    Création fonction create_column

--- functions.c
+++ functions.c
@@ -3,10 +3,14 @@
 //
 #include "functions.h"
 #include <stdlib.h>
+#include <string.h>
 
 COLUMN *create_column(char* title)
 {
-    int n=0;
-    char* title = calloc(n,sizeof(char));
-
+    COLUMN* new_column = (COLUMN*) malloc(sizeof(COLUMN));
+    strcpy(new_column->titre, title);
+    new_column->donnee = NULL;
+    new_column->TP = 0;
+    new_column->TL = 0;
+    return new_column;
 }
--- main.c
+++ main.c
@@ -1,3 +1,10 @@
 //
 // Created by HIM on 28/03/2024.
 //
+
+#include "functions.h"
+
+int main ()
+{
+    COLUMN* Mycolumn = create_column("My column");
+}
