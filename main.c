bc5f977702474509ceae17acc0607ebabbb32317
Victor de Laportalière <150955295+VictorTaillefer@users.noreply.github.com>
2024-04-12 17:53:21 +0200

    Commit de la structure COLUMN

--- main.c
+++ main.c
@@ -7,8 +7,11 @@ int main()
 {
     char* title = "colonne";
     COLUMN* col =  create_column(title);
-    int a = insert_value(col, 3);
-    printf("%d",a);
+    int val = 5;
+    if (insert_value(col, val))
+        printf("Value added successfully to my column\n");
+    else
+        printf("Error adding value to my column\n");
     return 0;
 }
 
