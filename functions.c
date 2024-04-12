aeda0717fdf0b1eef7e1ed02810d59fc81bdc5c6
Victor de Laportalière <150955295+VictorTaillefer@users.noreply.github.com>
2024-04-12 23:41:08 +0200

    correction de bugs sur les fonctions precedentes et ajout de la fonction print_col

--- functions.c
+++ functions.c
@@ -1,4 +1,4 @@
-
+#include <stdio.h>
 #include <stdlib.h>
 #include "functions.h"
 #include <string.h>
@@ -23,12 +23,16 @@ return new_column;
 int insert_value(COLUMN* col, int value)
 {
     int retour = 1;
+    /** verification des cas ou il est necessaire d'agrandir l'espace aloue*/
     if ((col->TP - col->TL == 0) || (col->donnee == NULL) )
     {
 
         if (col->donnee == NULL)
         {
-            col->donnee = calloc((col->TP+256),sizeof(int));
+            col->donnee = malloc((col->TP+256)*sizeof(int));
+            col->TP += 256;
+            col->donnee[col->TL] = value;
+            col->TL += 1;
         }
         else
         {
@@ -59,4 +63,16 @@ void delete_column(COLUMN **col)
 {
     free((*col)->donnee);
     free(*col);
+}
+
+
+/**
+* @brief: Print a column content
+* @param:  pointer to a column
+*/
+void print_col(COLUMN* col)
+{
+
+    for(int i=0;i<(col->TL);i++)
+        printf("[%d] %d\n",i,col->donnee[i]);
 }
--- main.c
+++ main.c
@@ -6,12 +6,20 @@
 int main()
 {
     char* title = "colonne";
-    COLUMN* col =  create_column(title);
+    COLUMN* col=NULL;
+    col =  create_column(title);
     int val = 5;
     if (insert_value(col, val))
         printf("Value added successfully to my column\n");
     else
         printf("Error adding value to my column\n");
+    print_col(col);
+    val = 52;
+    if (insert_value(col, val))
+        printf("Value added successfully to my column\n");
+    else
+        printf("Error adding value to my column\n");
+    print_col(col);
     delete_column(&col);
     return 0;
 }
