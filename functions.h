6df8df8d639abff23c6843b2f3db456f21ee9523
Victor de Laportalière <150955295+VictorTaillefer@users.noreply.github.com>
2024-05-06 15:46:52 +0200

    correction de bugs sur les fonctions precedentes et ajout de la fonction print_col

--- functions.h
+++ functions.h
@@ -1,13 +1,46 @@
-#ifndef FUNCTIONS_H
-#define FUNCTIONS_H
+//
+// Created by HIM on 28/03/2024.
+//
+
+#ifndef PROJET_CDATAFRAME__VICTOR_TAILLEFER_I_JAMES_HIM_FUNCTIONS_H
+#define PROJET_CDATAFRAME__VICTOR_TAILLEFER_I_JAMES_HIM_FUNCTIONS_H
+/**
+ * Create a column
+ * @param1 : Column title
+ * @return : Pointer to created column
+ */
+enum enum_type
+{
+NULLVAL = 1, UNIT, INT, CHAR, FLOAT, DOUBLE, STRING, STRUCTURE
+};
+typedef enum enum_type ENUM_TYPE;
+
+union column_type{
+unsigned int uint_value;
+signed int int_value;
+char char_value;
+float float_value;
+double double_value;
+char* string_value;
+void* struct_value;
+};
+typedef union column_type COL_TYPE ;
 
 typedef struct
 {
-    char* titre;
-    int TP;
-    int TL;
-    int* donnee;
+char* titre;
+signed int TP;
+unsigned int TL;
+int* donnee;
+COL_TYPE **data;
+unsigned long long int *index;
+ENUM_TYPE column_type;
 }COLUMN;
+
+typedef struct {
+COLUMN **column;
+int nb_column;
+}Cdataframe;
 
 COLUMN *create_column(char* title);
 
@@ -25,7 +58,34 @@ int nbr_occurence_sup(COLUMN* col, int x);
 
 int nbr_occurence_inf(COLUMN* col, int x);
 
+int add_column (Cdataframe *Cdata);
 
+Cdataframe *create_Cdataframe();
 
+void insert_value_endur(Cdataframe *Cdata, int nb_rows, int nb_columns, int (*values)[4]);
 
-#endif
+void lim_rows_display (Cdataframe *Cdata);
+
+void lim_column_display(Cdataframe *Cdata);
+
+void insert_values_user(Cdataframe *Cdata);
+
+void display_CDataframe(Cdataframe *Cdata);
+
+void nouveau_titre(Cdataframe *df);
+
+void verifier_valeur_existante(Cdataframe *df);
+
+void modifier_valeur_cellule(Cdataframe *df);
+
+void Affichage_nom_colonne(Cdataframe *df);
+
+void Affichage_nbr_ligne_colonne(Cdataframe *df);
+
+void analyser_valeur_x(Cdataframe *df);
+
+//PARTIE 2
+
+COLUMN *create_colum2(ENUM_TYPE type, char*title);
+
+#endif //PROJET_CDATAFRAME__VICTOR_TAILLEFER_I_JAMES_HIM_FUNCTIONS_H
--- main.c
+++ main.c
@@ -1,11 +1,19 @@
+//
+// Created by HIM on 28/03/2024.
+//
+
 #include <stdio.h>
-
-
 #include "functions.h"
+#include <string.h>
+#include <stdlib.h>
 
 int main()
 {
-    char* title = "colonne";
+    char *title=NULL;
+    title = (char*)malloc((strlen("colonne")+1)*sizeof(char));
+    strcpy(title, "colonne");
+    printf("%s",title);
+
     COLUMN* col=NULL;
     col =  create_column(title);
     int val = 5;
@@ -24,6 +32,102 @@ int main()
     printf("%d\n",nbr_occurence_sup(col,4));
     printf("%d\n",nbr_occurence_inf(col,6));
     delete_column(&col);
+    Cdataframe *df = create_Cdataframe();
+    if (df == NULL)
+    {  //verifie qu'il a été créer ou pas*
+        printf("erreur dans la création du  Cdataframe\n");
+    return 1;
+    }
+//int values[2][4] = {{1, 2, 3,4}, {5, 6, 7, 8}};
+// insert_value_endur(df, 2, 4, values);
+
+    int nombre = 0;
+
+    while (1)
+    { // Boucle infinie
+        printf("\nVoici le menu : \n");
+        printf("0) Quitter\n");
+        printf("1) Remplissage du CDataframe a partir de saisies utilisateurs\n");
+        printf("2) Afficher tout le CDataframe\n");
+        printf("3) Afficher une partie des lignes du CDataframe selon une limite fournie par l'utilisateur\n");
+        printf("4) Afficher une partie des colonnes du CDataframe selon une limite fournie par l'utilisateur\n");
+        printf("5) Ajouter une ligne de valeurs au CDataframe\n");
+        printf("6) Supprimer une ligne de valeurs du CDataframe\n");
+        printf("7) Ajouter une colonne au CDataframe\n");
+        printf("8) Supprimer une colonne du CDataframe\n");
+        printf("9) Renommer le titre d'une colonne du CDataframe\n");
+        printf("10) Verifier l'existence d'une valeur (recherche) dans le CDataframe\n");
+        printf("11) Acceder/remplacer la valeur se trouvant dans une cellule du CDataframe\n");
+        printf("12) Afficher les noms des colonnes\n");
+        printf("13) Afficher le nombre de lignes et de colonnes\n");
+        printf("14) Analyser une valeur x\n");
+        printf("\nSaisissez un nombre : ");
+
+        scanf("%d", &nombre);
+
+        switch (nombre) {
+            case 0:
+            // Quitter le programme
+            return 0;
+            case 1:
+                insert_values_user(df);
+                break;
+            case 2:
+                display_CDataframe(df);
+                break;
+            case 3:
+                lim_rows_display(df);
+                break;
+            case 4:
+                lim_column_display(df);
+                break;
+            case 5:
+                printf("Pas encore fait\n");
+                break;
+            case 6:
+                printf("Pas encore fait\n");
+                break;
+            case 7:
+                add_column(df);
+                break;
+            case 8: {
+                int column_value = 0;
+                while (column_value <= 0)
+                {
+                    printf("Enter the column value : ");
+                    scanf("%d", &column_value);
+                }
+                delete_column(&df->column[column_value]);
+                for (int i = column_value+1; i< df->nb_column; i ++)
+                {
+                    df->column[i-1]= df->column[i];
+                }
+                df->nb_column--;
+                df->column = realloc(df->column, df->nb_column * sizeof(COLUMN *));
+                break;
+                }
+            case 9:
+                nouveau_titre(df);
+                break;
+            case 10:
+                verifier_valeur_existante(df);
+                break;
+            case 11:
+                modifier_valeur_cellule(df);
+                break;
+            case 12:
+                Affichage_nom_colonne(df);
+                break;
+            case 13:
+                Affichage_nbr_ligne_colonne(df);
+                break;
+            case 14:
+                analyser_valeur_x(df);
+                break;
+            default:
+                printf("La saisie est invalide.\n");
+            break;
+        }
+    }
     return 0;
 }
-
